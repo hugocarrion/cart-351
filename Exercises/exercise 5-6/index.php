6079a8f3a4dc4e84f69018cb4716cd9edf1d1779
hugocarrion <31903454+hugocarrion@users.noreply.github.com>
2018-10-29 21:00:52 -0400

    Final
    Final changes

---
+++ Exercises/exercise 5-6/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Hello world example in php</title>
+</head>
+<body>
+<?php
+echo "Hello World!";
+?>
+</body>
+</html>
+<?php
+echo "Hello World!";
+
+echo "Hello World Again!";
+
+// has the same output as:
+echo "Hello World!";echo "Hello World Again!";
+?>
